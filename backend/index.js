8389559d5d7d54776095ec70f70647ce12b4685a
Sai Praneeth <93861208+praneethmcr@users.noreply.github.com>
2024-03-02 18:06:11 +0530

    frontend works

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ app.get("/customers", async (req, res) => {
   }
 });
 
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ backend/models/customerModel.js
@@ -0,0 +1,3 @@
+const pool = require("./connection");
+
+const customerData = "";
